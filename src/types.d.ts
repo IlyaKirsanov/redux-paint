209435f4144e96cdb10a42465d084506ae5afe08
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-12 13:23:55 +0300

    Prepare actions and root reducer

---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+export type Stroke = {
+	points: Point[],
+	color: string,
+	
+}
+
+export type Point = {
+	x: number,
+	y: number
+}
+
+export type RootState = {
+	currentStroke: Stroke
+	strokes: Stroke[]
+	historyIndex: number
+}
